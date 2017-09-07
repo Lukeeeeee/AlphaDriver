b3ffdb86df4e527852b6c366ebdd8e194acfcd5f
Lukeeeeee <lukedong123@gmail.com>
2017-09-07 15:32:50 +0800

    fix some bugs

--- src/model/actor/LSTMActor.py
+++ src/model/actor/LSTMActor.py
@@ -1,6 +1,7 @@
 from src.model.actor.actor import Actor
 import tensorlayer as tl
 import tensorflow as tf
+import tensorflow.contrib.layers as tf_contrib_layers
 
 
 class LSTMActor(Actor):
@@ -12,83 +13,102 @@ class LSTMActor(Actor):
     def create_model(self, state, name_prefix):
         W_init = tf.truncated_normal_initializer(stddev=0.01)
         b_init = tf.constant_initializer(value=0.0)
+
         state_shape = state.get_shape().as_list()
-        state = tf.reshape(tensor=state, shape=[-1, state_shape[2], state_shape[3], state_shape[4]])
-
         batch_size = state_shape[0]
-        state_length = state_shape[1]
 
-        inputs_image = tl.layers.InputLayer(inputs=state, name=name_prefix + 'INPUT_LAYER')
-        conv1 = tl.layers.Conv2d(net=inputs_image,
-                                 n_filter=self.config.config_dict['CONV1_1_CHANNEL_SIZE'],
-                                 filter_size=self.config.config_dict['CONV1_1_FILTER_SIZE'],
-                                 strides=self.config.config_dict['CONV1_1_STRIDE_SIZE'],
-                                 act=tf.nn.relu,
-                                 W_init=W_init,
-                                 b_init=b_init,
-                                 name=name_prefix + 'CONV1_1_LAYER'
-                                 )
-        conv1 = tl.layers.Conv2d(net=conv1,
-                                 n_filter=self.config.config_dict['CONV1_2_CHANNEL_SIZE'],
-                                 filter_size=self.config.config_dict['CONV1_2_FILTER_SIZE'],
-                                 strides=self.config.config_dict['CONV1_2_STRIDE_SIZE'],
-                                 act=tf.nn.relu,
-                                 W_init=W_init,
-                                 b_init=b_init,
-                                 name=name_prefix + 'CONV1_2_LAYER'
-                                 )
-        conv1 = tl.layers.BatchNormLayer(layer=conv1,
-                                         epsilon=0.000001,
-                                         is_train=self.is_training,
-                                         name=name_prefix + 'CONV1_BATCH_NORM_LAYER'
-                                         )
-        pool1 = tl.layers.MaxPool2d(net=conv1,
-                                    filter_size=(self.config.config_dict['POOL1_FILTER_SIZE']),
-                                    strides=self.config.config_dict['POOL1_STRIDE_SIZE'],
-                                    name=name_prefix + 'POOL1_LAYER'
-                                    )
-        conv2 = tl.layers.Conv2d(net=pool1,
-                                 n_filter=self.config.config_dict['CONV2_1_CHANNEL_SIZE'],
-                                 filter_size=self.config.config_dict['CONV2_1_FILTER_SIZE'],
-                                 strides=self.config.config_dict['CONV2_1_STRIDE_SIZE'],
-                                 act=tf.nn.relu,
-                                 W_init=W_init,
-                                 b_init=b_init,
-                                 name=name_prefix + 'CONV2_1_LAYER'
-                                 )
-        conv2 = tl.layers.Conv2d(net=conv2,
-                                 n_filter=self.config.config_dict['CONV2_2_CHANNEL_SIZE'],
-                                 filter_size=self.config.config_dict['CONV2_2_FILTER_SIZE'],
-                                 strides=self.config.config_dict['CONV2_2_STRIDE_SIZE'],
-                                 act=tf.nn.relu,
-                                 W_init=W_init,
-                                 b_init=b_init,
-                                 name=name_prefix + 'CONV2_2_LAYER'
-                                 )
-        conv2 = tl.layers.BatchNormLayer(layer=conv2,
-                                         epsilon=0.000001,
-                                         is_train=self.is_training.eval(),
-                                         name=name_prefix + 'CONV2_BATCH_NORM_LAYER'
-                                         )
-        pool2 = tl.layers.MaxPool2d(net=conv2,
-                                    filter_size=(self.config.config_dict['POOL2_FILTER_SIZE']),
-                                    strides=self.config.config_dict['POOL2_STRIDE_SIZE'],
-                                    name=name_prefix + 'POOL2_LAYER'
-                                    )
-        fc1 = tl.layers.DenseLayer(layer=pool2,
-                                   n_units=self.config.config_dict['DENSE_LAYER_1_UNIT'],
-                                   act=tf.nn.relu,
-                                   name=name_prefix + 'DENSE_LAYER_1_LAYER')
-        fc2 = tl.layers.DropconnectDenseLayer(layer=fc1,
-                                              n_units=self.config.config_dict['DENSE_LAYER_2_UNIT'],
-                                              act=tf.nn.sigmoid,
-                                              name=name_prefix + 'DENSE_LAYER_2_LAYER',
-                                              keep=self.config.config_dict['DROP_OUT_PROB_VALUE'])
+        state_list = tf.unstack(tf.transpose(state, [1, 0, 2, 3, 4]))
+        feature_list = []
 
-        lstm_input = tf.reshape(tensor=fc2.outputs,
-                                shape=[batch_size, self.config.config_dict['LSMT_INPUT_LENGTH'], -1])
+        for batch_image in state_list:
+
+            inputs_image = tl.layers.InputLayer(inputs=batch_image, name=name_prefix + 'INPUT_LAYER')
+            conv1 = tl.layers.Conv2d(net=inputs_image,
+                                     n_filter=self.config.config_dict['CONV1_1_CHANNEL_SIZE'],
+                                     filter_size=self.config.config_dict['CONV1_1_FILTER_SIZE'],
+                                     strides=self.config.config_dict['CONV1_1_STRIDE_SIZE'],
+                                     act=tf.nn.relu,
+                                     W_init=W_init,
+                                     b_init=b_init,
+                                     name=name_prefix + 'CONV1_1_LAYER'
+                                     )
+            conv1 = tl.layers.Conv2d(net=conv1,
+                                     n_filter=self.config.config_dict['CONV1_2_CHANNEL_SIZE'],
+                                     filter_size=self.config.config_dict['CONV1_2_FILTER_SIZE'],
+                                     strides=self.config.config_dict['CONV1_2_STRIDE_SIZE'],
+                                     act=tf.nn.relu,
+                                     W_init=W_init,
+                                     b_init=b_init,
+                                     name=name_prefix + 'CONV1_2_LAYER'
+                                     )
+            if self.is_training is True:
+                conv1 = tl.layers.BatchNormLayer(layer=conv1,
+                                                 epsilon=0.000001,
+                                                 is_train=True,
+                                                 name=name_prefix + 'CONV1_BATCH_NORM_LAYER'
+                                                 )
+            else:
+                conv1 = tl.layers.BatchNormLayer(layer=conv1,
+                                                 epsilon=0.000001,
+                                                 is_train=False,
+                                                 name=name_prefix + 'CONV1_BATCH_NORM_LAYER'
+                                                 )
+            pool1 = tl.layers.MaxPool2d(net=conv1,
+                                        filter_size=(self.config.config_dict['POOL1_FILTER_SIZE']),
+                                        strides=self.config.config_dict['POOL1_STRIDE_SIZE'],
+                                        name=name_prefix + 'POOL1_LAYER'
+                                        )
+            conv2 = tl.layers.Conv2d(net=pool1,
+                                     n_filter=self.config.config_dict['CONV2_1_CHANNEL_SIZE'],
+                                     filter_size=self.config.config_dict['CONV2_1_FILTER_SIZE'],
+                                     strides=self.config.config_dict['CONV2_1_STRIDE_SIZE'],
+                                     act=tf.nn.relu,
+                                     W_init=W_init,
+                                     b_init=b_init,
+                                     name=name_prefix + 'CONV2_1_LAYER'
+                                     )
+            conv2 = tl.layers.Conv2d(net=conv2,
+                                     n_filter=self.config.config_dict['CONV2_2_CHANNEL_SIZE'],
+                                     filter_size=self.config.config_dict['CONV2_2_FILTER_SIZE'],
+                                     strides=self.config.config_dict['CONV2_2_STRIDE_SIZE'],
+                                     act=tf.nn.relu,
+                                     W_init=W_init,
+                                     b_init=b_init,
+                                     name=name_prefix + 'CONV2_2_LAYER'
+                                     )
+            if self.is_training is True:
+                conv2 = tl.layers.BatchNormLayer(layer=conv2,
+                                                 epsilon=0.000001,
+                                                 is_train=True,
+                                                 name=name_prefix + 'CONV2_BATCH_NORM_LAYER'
+                                                 )
+            else:
+                conv2 = tl.layers.BatchNormLayer(layer=conv2,
+                                                 epsilon=0.000001,
+                                                 is_train=True,
+                                                 name=name_prefix + 'CONV2_BATCH_NORM_LAYER'
+                                                 )
+            pool2 = tl.layers.MaxPool2d(net=conv2,
+                                        filter_size=(self.config.config_dict['POOL2_FILTER_SIZE']),
+                                        strides=self.config.config_dict['POOL2_STRIDE_SIZE'],
+                                        name=name_prefix + 'POOL2_LAYER'
+                                        )
+            pool2 = tf_contrib_layers.flatten(inputs=pool2.outputs)
+
+            fc1 = tl.layers.InputLayer(inputs=pool2)
+            fc1 = tl.layers.DenseLayer(layer=fc1,
+                                       n_units=self.config.config_dict['DENSE_LAYER_1_UNIT'],
+                                       act=tf.nn.relu,
+                                       name=name_prefix + 'DENSE_LAYER_1_LAYER')
+            fc2 = tl.layers.DropconnectDenseLayer(layer=fc1,
+                                                  n_units=self.config.config_dict['DENSE_LAYER_2_UNIT'],
+                                                  act=tf.nn.sigmoid,
+                                                  name=name_prefix + 'DENSE_LAYER_2_LAYER',
+                                                  keep=self.config.config_dict['DROP_OUT_PROB_VALUE'])
+            feature_list.append(fc2)
 
         # LSTM INPUT IS [BATCH_SIZE, LENGTH, FEATURE_DIM]
+        lstm_input = tf.stack(feature_list, axis=1)
 
         # TODO
         # be aware of the init_state when train a lstm
